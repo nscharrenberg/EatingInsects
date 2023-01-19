f4ad62a4961229d8ea30001b858331889ea17719
clausanchezm <37293827+clausanchezm@users.noreply.github.com>
2023-01-19 14:39:06 +0100

    rfs

---
+++ app/networks/RF/random_forest.py
@@ -0,0 +1,26 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
+from app.networks.shared.BaseNetwork import BaseNetwork
+
+
+class RF(BaseNetwork):
+    def compile(self):
+        if self.model:
+            raise Exception("Model has already been compiled")
+
+        self.model = RandomForestRegressor(n_estimators=100, random_state=0)
+        return self.model
+
+    def train(self):
+        self.model.fit(
+            self.train_features,
+            self.train_labels,
+        )
+
+    def test(self):
+        predictions = self.model.predict(self.test_features)
+        self.results = []
+        self.results.append(sqrt(metrics.mean_squared_error(self.test_labels, predictions)))
+        self.results.append(metrics.mean_absolute_error(self.test_labels, predictions))
+
+        return self.results
