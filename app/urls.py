6df115f0b3d22873778b9e692ae7b85ddc8eb24a
Parand <p.mohri@student.maastrichtuniversity.nl>
2023-01-25 13:56:54 +0100

    add experiments page

--- app/urls.py
+++ app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('models/update/<slug:slug>', views.UpdateModelView.as_view(), name='update_model'),
     path('models/delete/<slug:slug>', views.DeleteModelView.as_view(), name='delete_model'),
     path('models', views.ModelsOverviewView.as_view(), name='all_model'),
-    path('', views.PredictionView.as_view(), name="make_prediction")
+    path('', views.PredictionView.as_view(), name="make_prediction"),
+    path('experiments', views.ExperimentsView.as_view(), name="experiments")
 ]
